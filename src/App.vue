f8c76acee1723c17d22c203b1d979868f1f935c1
newtonkim <knewts98@gmail.com>
2022-02-16 15:15:40 +0300

    add taiwind and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Navbar />
+
+    <router-view />
+    <footer>footer</footer>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navbar from "@/components/layouts/Navbar";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="navbar" class="px-4 py-4 sm:py-6 bg-white shadow sm:fixed w-full z-10">
+    <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
